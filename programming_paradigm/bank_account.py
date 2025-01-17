a5bc0dbb06f44db99b801063777501e13fed901a
dev-dede <tsewayog@gmail.com>
2025-01-17 20:39:42 +0000

    Implement BankAccount class with deposit, withdraw, and display_balance methods

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,13 @@
+class BankAccount:
+    def __init__(self, account_balance = 0):
+        self.__account_balance = account_balance
+    def deposit(self, amount):
+        self.__account_balanceaccount_balance += amount
+    def withdraw(self, amount):
+        if amount < self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
