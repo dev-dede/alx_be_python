1e59cfeb147ca70eac2e4bf405dc1ed968fd655f
dev-dede <tsewayog@gmail.com>
2025-01-18 16:30:52 +0000

    Further rename more method names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,12 +14,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(5, 10), -5)
         self.assertEqual(self.calc.subtract(-10, -5), -5)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
-    def test_multiply(self):
+    def test_multiplicatiion(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-2, -3), 6)
         self.assertEqual(self.calc.multiply(2, -3), -6)
         self.assertEqual(self.calc.multiply(2, 0), 0)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(3, 2), 1.5)
         self.assertEqual(self.calc.divide(0, 2), 0)
         self.assertEqual(self.calc.divide(9.6, 2), 4.8)
