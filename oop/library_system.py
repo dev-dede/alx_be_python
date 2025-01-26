08c73dc7211ef3ae6b5625c21541b74a4671e2f9
dev-dede <tsewayog@gmail.com>
2025-01-26 15:08:52 +0000

    Add KB at the end of file size in the print statement for ebooks to know the unit of the file size

--- oop/library_system.py
+++ oop/library_system.py
@@ -22,7 +22,7 @@ class Library:
         book_list = []
         for book in self.books:
             if isinstance (book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
@@ -31,7 +31,7 @@ class Library:
     def list_books(self):
         for book in self.books:
             if isinstance (book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
             elif isinstance(book, PrintBook):
                 print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
             else:
