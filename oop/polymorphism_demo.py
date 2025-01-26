9bbd9aecb966fa7e390f2e921ce8715102820c90
dev-dede <tsewayog@gmail.com>
2025-01-26 20:35:34 +0000

    Add script that demonstrates inheritance and polymorphism

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,16 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("The 'area' method must be implemented by a subclass.")
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * self.radius * self.radius
