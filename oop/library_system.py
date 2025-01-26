90878dd8605bc4439b818a8918e682f8278f4216
dev-dede <tsewayog@gmail.com>
2025-01-26 15:05:10 +0000

    Add string representation format for checker requirements

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,6 +18,16 @@ class Library:
             self.books.append(book)
         else:
             print("Only instances of Book, EBook or PrintBook can be added to the library")
+    def __str__(self):
+        book_list = []
+        for book in self.books:
+            if isinstance (book, EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}")
+            elif isinstance(book, PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
+        return "/n".join(book_list)
     def list_books(self):
         for book in self.books:
             if isinstance (book, EBook):
