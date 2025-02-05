ab4a52abd8fa3f3c92d16316b9441bd7859b77ab
dev-dede <tsewayog@gmail.com>
2025-02-05 15:31:29 +0000

    Add basic python calculator script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,12 @@
-def perform_operation(num1, num2, operation):
-    if operation == "add":
-        return num1 + num2
-    elif operation == "subtract":
-        return num1 - num2
-    elif operation == "multiply":
-        return num1 * num2
-    elif operation == "division":
-        if num2 == 0:
-            return "Cannot divide by zero"
-        return num1 / num2
-    else:
-        return "Invalid operation"
+def perform_operation(num1: float, num2: float, operation: str):
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            return num1 / num2 if num2 != 0 else "Cannot divide by zero"
+        case _:
+            return "Invalid operation"
