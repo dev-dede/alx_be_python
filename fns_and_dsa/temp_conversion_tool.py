5c456f4c444e00462c39e5589bfcffd21261bda4
dev-dede <tsewayog@gmail.com>
2025-02-08 16:45:12 +0000

    Add python script for temperature conversion

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,35 +1,28 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
-CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return celsius
+    print(f"{fahrenheit}°F is {celsius}°C")
 
 def convert_to_fahrenheit(celsius):
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-    return fahrenheit
-
-
+    print(f"{celsius}°C is {fahrenheit}°F")
 
 def main():
     try:
         temperature = float(input("Enter the temperature to convert: "))
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").lower()
+    
+    
+        if unit == "c":
+            convert_to_fahrenheit(temperature)
+        elif unit == "f":
+            convert_to_celsius(temperature)
+        else:
+            print("Invalid unit. Select c or f")
     except ValueError:
         print("Invalid temperature. Please enter a numeric value.")
-        return
-    
-    temperature_scale = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().capitalize()
-    
-    if temperature_scale == "F":
-        result = convert_to_celsius(temperature)
-        print(f"{temperature}°F is {result}°C")
-    elif temperature_scale == "C":
-        result = convert_to_fahrenheit(temperature)
-        print(f"{temperature}°C is {result}°F")
-    else:
-        print("Invalid temperature scale! Please enter either 'C' or 'F'.")
-        
-
 
 if __name__ == "__main__":
     main()
